a621e60dcccd1d831b371b7ca300360bdc593f6e
Wilhelm-Koltermann <w.koltermann@googlemail.com>
2017-11-29 22:22:57 +0100

    bis Aufgabe 2 in Versuch 4

--- CLionProjects/Labor_4/main.c
+++ CLionProjects/Labor_4/main.c
@@ -5,19 +5,19 @@
 
 //Aufgabe 1 Vorbereitung: Entfernung zweier Punkte
 
-struct point {
+struct POINT {
 
     double x;
     double y;
 };
 
 
-double distance_of_points(struct point p1, struct point p2);
+double distance_of_points(struct POINT p1, struct POINT p2);
 
-int main() {
+int main1V() {
 
 
-    struct point x = {1., 1.}, y = {2., 2.}, z = {-2., -1.};
+    struct POINT x = {1., 1.}, y = {2., 2.}, z = {-2., -1.};
 
     printf("Die Entfernung von x und y betraegt: %.2lf\n", distance_of_points(x, y));
 
@@ -29,11 +29,65 @@ int main() {
 }
 
 
+//Aufgabe 2 Vorbereitung
+
+struct SEGMENT calculate_segment_v1(struct SEGMENT s);
+
+void print_segment(struct SEGMENT s);
+
+struct SEGMENT read_segment_v1();
+
+struct SEGMENT {
+
+    struct POINT p1;
+    struct POINT p2;
+    double length;
+    double angle;
+};
+
+
+int main() {
+
+    struct SEGMENT S;
+
+    char c;
+
+    double l;
+
+    while (1) {
+
+
+        printf("Bitte # eingeben fuer Programmende: ");
+
+        fflush(stdin);
+
+        scanf("%c", &c);
+
+        if (c == '#') break;
+
+        S = read_segment_v1();
+
+        S = calculate_segment_v1(S);
+
+        print_segment(S);
+    }
+
+    printf("\nEnde des Programms\n");
+
+    return 0;
+}
+
 //Funktionen
+
+//Funktion grad aus Versuch 3
+
+double grad (double rad) {
+    return (rad*45.) / atan(1.);
+}
 
 //Funktion Aufgabe 1 Vorbereitung: Entfernung zweier Punkte
 
-double distance_of_points(struct point p1, struct point p2) {
+double distance_of_points(struct POINT p1, struct POINT p2) {
 
     double xk, yk;
 
@@ -43,5 +97,60 @@ double distance_of_points(struct point p1, struct point p2) {
 
     return sqrt(pow(xk, 2) + pow(yk, 2));
 
+
+}
+
+//Funktion Aufgabe 2 Vorbereitung: print_segment
+
+void print_segment(struct SEGMENT s) {
+
+    printf("\n--> Die Strecke verbindet die Punkte"
+                   " (%.2lf,%.2lf) und (%.2lf,%.2lf);\n"
+                   "--> Sie hat eine Laenge von %.4lf;\n"
+                   "--> Der Winkel betraegt: %.2lf Grad.\n\n",
+
+           s.p1.x, s.p1.y, s.p2.x, s.p2.y, s.length, s.angle);
+}
+
+
+//Funktion Aufgabe 2 Vorbereitung: read_segment
+
+struct SEGMENT read_segment_v1() {
+
+    struct SEGMENT s;
+
+
+    printf("\n>> Bitte Koordinaten x,y des ersten"
+                   " Punktes der Strecke eingeben: ");
+
+
+    scanf("%lf %lf", &s.p1.x, &s.p1.y);
+
+
+    printf(">> Bitte Koordinaten x,y des zweiten"
+                   " Punktes der Strecke eingeben: ");
+
+
+    scanf("%lf %lf", &s.p2.x, &s.p2.y);
+
+
+    return s;
+}
+
+//Funktion Aufgabe 2 Vorbereitung: calculate_segment_v1
+
+struct SEGMENT calculate_segment_v1(struct SEGMENT s) {
+
+    double x,y;
+
+    x=s.p2.x - s.p1.x;
+
+    y=s.p2.y - s.p1.y;
+
+    s.length = distance_of_points(s.p1,s.p2);
+
+    s.angle= grad(atan2(y,x));
+
+    return s;
 
 }
