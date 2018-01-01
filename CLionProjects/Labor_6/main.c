613b22f4b5213a4ed0a19011a4886e99bc77df68
Wilhelm-Koltermann <w.koltermann@googlemail.com>
2018-01-01 23:17:20 +0100

    Aufgabe 1 mit Problemen

--- CLionProjects/Labor_6/main.c
+++ CLionProjects/Labor_6/main.c
@@ -3,8 +3,11 @@
 #include "polynom.h"
 #include "simple_rgb_image.h"
 #include <stdlib.h>
+#include <math.h>
 
 //Augabe 1: Bilddatei erstellen
+
+double distance(double xm, double ym, double x, double y);
 
 int main(void)
 {
@@ -59,7 +62,7 @@ int main(void)
 
 //Funktionen
 
-//Funktion Vorbereitung 1:
+//Funktion Vorbereitungsaufgabe 1: Initialisierung des Bildes
 
 void simple_rgb_image_init(simple_rgb_image_t *sink, int width, int height) {
 
@@ -69,4 +72,83 @@ void simple_rgb_image_init(simple_rgb_image_t *sink, int width, int height) {
 
     sink->data = malloc(3 * width * height);
 
+    if (sink->data == NULL) { printf("Neuen Wert für Obergrenze eingeben\n"); }
+}
+
+//Funktion Vorbereitungsaufgabe 1: Freigabe des Speicherplatzes
+
+void simple_rgb_image_clear(simple_rgb_image_t *sink){
+
+    free(sink->data);
+}
+
+//Funktion Vorbereitungsaufgabe 1: Erzeugung der Bilddatei
+
+void simple_rgb_image_to_bitmap_stream(simple_rgb_image_t *source, FILE *stream){
+
+    int i;
+
+    for(i=0;i<= (3*source->width*source->height);i++) {
+
+        fputc(source->data[i], stream);
+    }
+
+}
+
+//Funktion Vorbereitungsaufgabe 1: Pixel Färben
+
+void simple_rgb_image_put_pixel(simple_rgb_image_t *sink, int x, int y,
+                                unsigned char red, unsigned char green, unsigned char blue)
+{
+
+    sink->data[x] = red;
+    sink->data[y] = red;
+    sink->data[x+1] = green;
+    sink->data[y+1] = green;
+    sink->data[x+2] = blue;
+    sink->data[y+2] = blue;
+
+}
+
+//Funktion Vorbereitunsgaugabe 1: Linie einzeichnen
+
+void simple_rgb_image_draw_line(simple_rgb_image_t *sink, int x0, int y0, int
+x1, int y1, int width, unsigned char red, unsigned char green, unsigned char blue)
+{
+    int i,j;
+
+    for(i=x0,j=y0;i<=x1,j<=y1;i=x0++,j=y0++) {
+
+        simple_rgb_image_put_pixel(sink,i,j+width,red,green,blue);
+    }
+}
+
+
+//Funktion Vorbereitungsaufgabe 1: Kreis zeichnen und ausfüllen
+
+void simple_rgb_image_fill_circle(simple_rgb_image_t *sink, int x, int y, int
+radius, unsigned char red, unsigned char green, unsigned char blue){
+
+    int i,j;
+
+    for(i=0,j=0;i<=sink->height-1,j<=sink->width-1;i++,j++)
+
+        if(distance(x,y,i,j)<=radius) {simple_rgb_image_put_pixel(sink,i,j,red,green,blue);}
+    }
+
+
+
+
+//Funktion Distance: Aufgabe2 Vorbereitung und Labor, Abstand zweier Punkte
+//Bib:math.h
+
+double distance(double xm, double ym, double x, double y) {
+
+    double Sub_x2_x1 = x - xm;   //Subtraktion der x-Koordinaten
+
+    double Sub_y2_y1 = y - ym;   //"--" y-Koordinaten
+
+    double vektorBetrag = sqrt(pow(Sub_x2_x1, 2) + pow(Sub_y2_y1, 2));
+
+    return vektorBetrag;
 }
